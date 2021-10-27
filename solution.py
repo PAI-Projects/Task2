b3fe61e17b654ff5bfdaf912423678493cffd37c
braunjon <25931090+braunjon@users.noreply.github.com>
2021-10-27 17:34:57 +0200

    move models

--- solution.py
+++ solution.py
@@ -1,17 +1,15 @@
 import os
-import typing
 
 import numpy as np
 import torch
 import torch.optim
 from matplotlib import pyplot as plt
 from sklearn.metrics import roc_auc_score, average_precision_score
-from torch import nn
 from torch.nn import functional as F
 from tqdm import trange
 
-from layers import BayesianLayer
-from util import ece, ParameterDistribution
+from models import DenseNet, BayesNet
+from util import ece
 
 # Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
 EXTENDED_EVALUATION = False
@@ -156,69 +154,6 @@ class Model(object):
         return output
 
 
-class BayesNet(nn.Module):
-    """
-    Module implementing a Bayesian feedforward neural network using BayesianLayer objects.
-    """
-
-    def __init__(self, in_features: int, hidden_features: typing.Tuple[int, ...], out_features: int):
-        """
-        Create a BNN.
-
-        :param in_features: Number of input features
-        :param hidden_features: Tuple where each entry corresponds to a (Bayesian) hidden layer with
-            the corresponding number of features.
-        :param out_features: Number of output features
-        """
-
-        super().__init__()
-
-        feature_sizes = (in_features,) + hidden_features + (out_features,)
-        num_affine_maps = len(feature_sizes) - 1
-        self.layers = nn.ModuleList([
-            BayesianLayer(feature_sizes[idx], feature_sizes[idx + 1], bias=True)
-            for idx in range(num_affine_maps)
-        ])
-        self.activation = nn.ReLU()
-
-    def forward(self, x: torch.Tensor) -> typing.Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Perform one forward pass through the BNN using a single set of weights
-        sampled from the variational posterior.
-
-        :param x: Input features, float tensor of shape (batch_size, in_features)
-        :return: 3-tuple containing
-            i) output features using stochastic weights from the variational posterior,
-            ii) sample of the log-prior probability, and
-            iii) sample of the log-variational-posterior probability
-        """
-
-        # TODO: Perform a full pass through your BayesNet as described in this method's docstring.
-        #  You can look at DenseNet to get an idea how a forward pass might look like.
-        #  Don't forget to apply your activation function in between BayesianLayers!
-        log_prior = torch.tensor(0.0)
-        log_variational_posterior = torch.tensor(0.0)
-        output_features = None
-
-        return output_features, log_prior, log_variational_posterior
-
-    def predict_probabilities(self, x: torch.Tensor, num_mc_samples: int = 10) -> torch.Tensor:
-        """
-        Predict class probabilities for the given features by sampling from this BNN.
-
-        :param x: Features to predict on, float tensor of shape (batch_size, in_features)
-        :param num_mc_samples: Number of MC samples to take for prediction
-        :return: Predicted class probabilities, float tensor of shape (batch_size, 10)
-            such that the last dimension sums up to 1 for each row
-        """
-        probability_samples = torch.stack([F.softmax(self.forward(x)[0], dim=1) for _ in range(num_mc_samples)], dim=0)
-        estimated_probability = torch.mean(probability_samples, dim=0)
-
-        assert estimated_probability.shape == (x.shape[0], 10)
-        assert torch.allclose(torch.sum(estimated_probability, dim=1), torch.tensor(1.0))
-        return estimated_probability
-
-
 def evaluate(model: Model, eval_loader: torch.utils.data.DataLoader, data_dir: str, output_dir: str):
     """
     Evaluate your model.
@@ -357,50 +292,6 @@ def evaluate(model: Model, eval_loader: torch.utils.data.DataLoader, data_dir: s
             'AUPRC for MNIST vs. FashionMNIST OOD detection based on confidence: '
             f'{average_precision_score(dataset_labels, all_confidences):.3f}'
         )
-
-
-class DenseNet(nn.Module):
-    """
-    Simple module implementing a feedforward neural network.
-    You can use this model as a reference/baseline for calibration
-    in the normal neural network case.
-    """
-
-    def __init__(self, in_features: int, hidden_features: typing.Tuple[int, ...], out_features: int):
-        """
-        Create a normal NN.
-
-        :param in_features: Number of input features
-        :param hidden_features: Tuple where each entry corresponds to a hidden layer with
-            the corresponding number of features.
-        :param out_features: Number of output features
-        """
-        super().__init__()
-
-        feature_sizes = (in_features,) + hidden_features + (out_features,)
-        num_affine_maps = len(feature_sizes) - 1
-        self.layers = nn.ModuleList([
-            nn.Linear(feature_sizes[idx], feature_sizes[idx + 1], bias=True)
-            for idx in range(num_affine_maps)
-        ])
-        self.activation = nn.ReLU()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        current_features = x
-
-        for idx, current_layer in enumerate(self.layers):
-            new_features = current_layer(current_features)
-            if idx < len(self.layers) - 1:
-                new_features = self.activation(new_features)
-            current_features = new_features
-
-        return current_features
-
-    def predict_probabilities(self, x: torch.Tensor) -> torch.Tensor:
-        assert x.shape[1] == 28 ** 2
-        estimated_probability = F.softmax(self.forward(x), dim=1)
-        assert estimated_probability.shape == (x.shape[0], 10)
-        return estimated_probability
 
 
 def main():
